e2e1b8ecd00404eca29972036d3107365fbd655f
Ivan Porollo <iporollo@gmail.com>
2023-01-30 22:40:05 -0800

    add coworking link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -205,6 +205,48 @@ export default function Home() {
               </svg>
             </a>
           </li>
+          <li className=" border border-gray-400 rounded-lg hover:bg-gray-200 mb-4">
+            <a
+              href="https://airtable.com/shr33svLXILbTf1bs"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center p-4 flex justify-between items-center"
+            >
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="rounded-md pr-4"
+                  style={{ height: 'auto', width: 64, color: 'gray' }}
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"
+                  />
+                </svg>
+                SF AI Builders Co-Working
+              </div>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                />
+              </svg>
+            </a>
+          </li>
         </ul>
       </main>
     </>
